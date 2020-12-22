c28a62f2b9ddae7bee4217d96a26d800da40878b
Vipul Chauhan <viratdev07@gmail.com>
2020-12-22 21:42:00 +0530

    Add Controller and CreateRequest Pojo

--- src/main/java/com/example/springmongo4/demo/mongo4/BookController.java
+++ src/main/java/com/example/springmongo4/demo/mongo4/BookController.java
@@ -1,4 +1,30 @@
 package com.example.springmongo4.demo.mongo4;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/getBooks")
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/getBookByAuthor")
+    public List<Book> getBook(@RequestParam(value="name") String name) {
+           return bookRepository.findByAuthorName(name);
+    }
+
+    @PostMapping("/insertBook")
+    public void insertBook(@RequestBody CreateRequest request) {
+
+        Book book = new Book(request.getName(), request.getAuthorName(), request.getCost());
+        bookRepository.save(book);
+    }
 }
--- src/main/java/com/example/springmongo4/demo/mongo4/BookRepository.java
+++ src/main/java/com/example/springmongo4/demo/mongo4/BookRepository.java
@@ -2,6 +2,10 @@ package com.example.springmongo4.demo.mongo4;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BookRepository extends MongoRepository<Book, Integer> {
+
+    public List<Book> findByAuthorName(String name);
 
 }
