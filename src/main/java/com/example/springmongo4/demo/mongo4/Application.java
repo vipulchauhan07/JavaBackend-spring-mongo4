f9173336891c67e1909bef651c62d5a02feb2ce8
Vipul Chauhan <viratdev07@gmail.com>
2020-12-24 05:58:37 +0530

    Add swagger2 tool

--- src/main/java/com/example/springmongo4/demo/mongo4/Application.java
+++ src/main/java/com/example/springmongo4/demo/mongo4/Application.java
@@ -2,7 +2,9 @@ package com.example.springmongo4.demo.mongo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
